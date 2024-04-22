7e4c7f56cd1ba05d3cbb7e55f2b5e9b75c3d270e
scottyboii <scott.butler42@gmail.com>
2024-04-22 09:40:47 +0100

    Initial commit

--- app/src/main/java/com/example/httpurlconnectionexample/MainActivity.java
+++ app/src/main/java/com/example/httpurlconnectionexample/MainActivity.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    String url_api_view = "http://tutor.csucleeds.com/tutor/cpu/api.php?apicall=view";
+    String url_api_view = "https://student03.csucleeds.com/student03/cpu/api.php?apicall=view";
 
     FloatingActionButton fab;
 
